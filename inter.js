efe6adfe62d9b9c6aebe6e820a5c2f809c9c6cf5
anna <anna.lokietko@gmail.com>
2020-03-01 23:02:35 +0100

    ulepszenia

--- inter.js
+++ inter.js
@@ -1,5 +1,35 @@
-console.log("hello");
 
 
+
+
+document.addEventListener("DOMContentLoaded", function () {
+   
+    var x = document.getElementById("od");
+    var y = document.getElementById("do");
+
+    function otw(){
+var nd=document.querySelector("div.zielony")
+
+        var dzisiaj = new Date();
+        var dzien = dzisiaj.getDay();
+        if(dzien==0){
+            nd.innerText=" Dzisiaj zamknięte, zapraszamy jutro od 17:00";
+            nd.style.color= "red";
+        }
+        if(dzien==6){
+            x.innerText="10:00";
+            y.innerText="21:00";
+        }
+        else{
+            x.innerText="17:00";
+            y.innerText="21:00";
+        }
+    }
+
+
+
+ otw();
+
+})
 
 
